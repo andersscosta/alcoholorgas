378c33060a7151804a040a5d5e48da0c65038ab0
Anderson Costa <andersscosta2@gmail.com>
2021-11-11 09:49:35 -0300

    first commit

---
+++ ag.js
@@ -0,0 +1,7 @@
+let alc = prompt('Qual o preço do álcool?')
+let gas = prompt('Qual o preço da gasolina?')
+if(alc/gas <= 0.700){
+    alert('Abasteça com ÁLCOOL')
+}else{
+    alert('Abasteça com GASOLINA')
+}
